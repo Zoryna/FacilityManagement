f27f9bfc03f8eb99be52f7e04ecf58e6263c20df
Lucas Perez <knightfication@gmail.com>
2019-01-30 22:58:45 -0600

    Files Added

---
+++ Control.java
@@ -0,0 +1,49 @@
+/*
+  class that determines if a facility is available and assigns them
+  also schedules if facility needs maintenance
+*/
+
+
+
+public class Control
+{
+  int interval;
+  
+
+  public boolean assignToUse()
+  {
+
+
+
+  }
+
+  public boolean vacateFacility()
+  {
+
+
+
+  }
+
+  public boolean scheduleMaintenance()
+  {
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ FacilityControl.java
@@ -0,0 +1,60 @@
+/*
+
+*/
+
+public class FacilityControl
+{
+
+
+
+  public int addNewFacility()
+  {
+
+
+  }
+
+  public int removeFacility()
+  {
+
+
+  }
+
+  public int editNumber()
+  {
+
+
+  }
+
+  public String editDescription()
+  {
+
+
+  }
+
+  public int editCost()
+  {
+
+
+  }
+
+  public String editAddress()
+  {
+
+
+  }
+
+  public int editCapacity()
+  {
+
+    
+  }
+
+
+
+
+
+
+
+
+
+}
