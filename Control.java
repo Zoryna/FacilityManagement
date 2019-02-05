3162db5d0378c1392b71be9ca78fce4eabf7fade
Lucas Perez <knightfication@gmail.com>
2019-02-04 18:50:12 -0600

    Some pseudo code for Janeen

--- Control.java
+++ Control.java
@@ -1,49 +1,26 @@
 /*
-  class that determines if a facility is available and assigns them
-  also schedules if facility needs maintenance
+    The control class has the goal to change the state of the facilities according
+    to the rules of when they are WORKING, RESTING, or MAINTENANCE
 */
 
-
+// Fetch the facility objects, and change their states using these three functions
 
 public class Control
 {
-  int interval;
-  
+    int interval;
 
-  public boolean assignToUse()
-  {
+    public boolean assignToUse()
+    {
+        // This function changes the facility to State.WORKING
+    }
 
+    public boolean vacateFacility()
+    {
+        // Assign a facility to RESTING state
+    }
 
-
-  }
-
-  public boolean vacateFacility()
-  {
-
-
-
-  }
-
-  public boolean scheduleMaintenance()
-  {
-
-
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public boolean scheduleMaintenance()
+    {
+        // Assign a facility to MAINTENANCE
+    }
 }
--- Inspection.java
+++ Inspection.java
@@ -1,37 +1,31 @@
 /*
-  observer that inspects all the facilities
-  what needs to be inspected
-  and makes a request to what needs to be maintained
-  Inspection-->Maintenance
+    1. Check on what needs to be inspected, probably Facilities only
+    2. Talks to the interested parties about what's going on (Is the Facility working? Is it at Maintenance? Is it resting? Should it be resting?)
 */
-
-
 
 public class Inspection
 {
-  //List
+    public void listFacilityIssues()
+    {
+        // Our facilities just do one thing now, they do work or they do not, so at this moment,
+        // this isn't needed, unless you wanna assign facilities to a Queue when they have
+        // the MAINTENANCE state.
+    }
 
+    public void makeMaintenanceRequest()
+    {
+        // Connect to Maintenance, obviously
+        /*
+            - You will need a Driver with many facilities with different states, just create a scenario of it
+            - Write a Inspection function that checks the facility state, select the ones that are RESTING
+            - return something that the Maintenance class can do with it
+        */
+    }
 
-  public String listFacilityIssues()
-  {
-
-
-  }
-
-  public boolean makeMaintenanceRequest()
-  {
-
-
-  }
-
-  public List listInspections()
-  {
-
-
-  }
-
-
-
-
-
+    public void listInspections()
+    {
+        // Kind of the same thing, what could be done here is to call the dates of the
+        // inspections done and use the WriteFile (or w/e it's called) to save the inspections
+        // that have been done 
+    }
 }
