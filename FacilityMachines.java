de791d5552cb341266f2c3649f227cf79ca2190e
Lucas Perez <knightfication@gmail.com>
2019-02-10 01:55:21 -0600

    FacilityMachines added

---
+++ FacilityMachines.java
@@ -0,0 +1,62 @@
+public class FacilityMachines extends Facility{
+
+    private boolean oxygenator, innerAirlocks, externalAirlocks, solarPanels, transLine,
+    comms, nuclearReactor, waterMaking;
+    private final int machines = 7; // Number of machines
+    // Store a Hashmap with the keys as the variables, and return it.
+    // Maybe extending the StateMachine is needed
+
+    FacilityMachines() {
+        super(name, refNumber);
+    }
+
+    public void setNuclearReactor(boolean nuclearReactor){
+        this.nuclearReactor = nuclearReactor;
+    }
+
+    public void setInnerAirlocks(boolean innerAirlocks){
+        this.innerAirlocks = innerAirlocks;
+    }
+
+    public void setExternalAirlocks(boolean externalAirlocks){
+        this.externalAirlocks = externalAirlocks;
+    }
+
+    public void setSolarPanels(boolean solarPanels){
+        this.solarPanels = solarPanels;
+    }
+
+    public void setComms(boolean comms){
+        this.comms = comms;
+    }
+
+    public void setWaterMaking(boolean waterMaking){
+        this.waterMaking = waterMaking;
+    }
+
+    public void setoxygenator(boolean oxygenator){
+        this.oxygenator = oxygenator;
+    }
+
+    public void checkMachines(){
+        int[] ar = new int[machines];
+        System.out.println("Oxygenator: " + oxygenator);
+        System.out.println("Inner Airlocks: " + innerAirlocks);
+        System.out.println("External Airlocks: " + externalAirlocks);
+        System.out.println("Solar Panels: " + solarPanels);
+        System.out.println("Comms: " + comms);
+        System.out.println("Water Making: " + waterMaking);
+    }
+
+    public static void main (String[] args){
+        FacilityMachines fm = new FacilityMachines();
+        boolean state = true;
+        fm.setInnerAirlocks(state);
+        fm.setExternalAirlocks(state);
+        fm.setSolarPanels(state);
+        fm.setComms(state);
+        fm.setWaterMaking(state);
+        System.out.println("Oxygenator: " + fm.getOxygenator());
+        fm.checkMachines();
+    }
+}
