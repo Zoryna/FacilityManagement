102a32f439332dfabbcfba4960265f23ec08aaf7
Lucas Perez <knightfication@gmail.com>
2019-02-23 15:04:45 -0600

    Packages are done

---
+++ marsmission/dataaccess/WriteFile.java
@@ -0,0 +1,102 @@
+package marsmission.dataaccess;
+import marsmission.habcontrol.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+
+public class WriteFile {
+
+    public String fileName;
+    FacilityMachines fm = new FacilityMachines();
+    private Map<String, Boolean> m = new HashMap<String, Boolean>();
+    PrintWriter oStream = null;
+
+    public void setFileName(String fileName){
+        this.fileName = fileName;
+    }
+
+    public String getFileName(){
+        return fileName;
+    }
+
+    // Copies static map from FacilityMachines to WriteFile
+    public void copyMap() {
+        for (Map.Entry<String, Boolean> i: fm.getMap().entrySet()){
+            m.put(i.getKey(), i.getValue());
+        }
+    }
+
+    public void saveFacilityInfo(Facility fac){
+
+        try {
+            oStream = new PrintWriter(getFileName());
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Error saving the file " + fileName + ". I can't provide more information because I'm a computer. Beep Boop.");
+            System.exit(0);
+        }
+
+        /*
+            This thing can probably be improved A LOT, but needs a total revamp,
+            let's ignore how ugly this is for now? Please and thank you. But hey,
+            this still complies to SRP. Silver lining. (ღ˘⌣˘ღ)
+        */
+
+        int i = 0;
+        String name = fac.getName();
+        oStream.println(i + ": " + name + ".");
+        i++;
+        int refNumber = fac.getRefNumber();
+        oStream.println(i + ": " + refNumber + ".");
+        i++;
+        String address = fac.getAddress();
+        oStream.println(i + ": " + address + ".");
+        i++;
+        String description = fac.getDescription();
+        oStream.println(i + ": " + description + ".");
+        i++;
+        int capacity = fac.getCapacity();
+        oStream.println(i + ": " + capacity + ".");
+        i++;
+        double cost = fac.getCost();
+        oStream.println(i + ": " + cost + ".");
+        i++;
+        int problemRate = fac.getProblemRate();
+        oStream.println(i + ": " + problemRate + ".");
+        i++;
+
+        copyMap();
+        for (Map.Entry<String, Boolean> j: m.entrySet()){
+            String s = j.getKey();
+            boolean b = j.getValue();
+            oStream.println(i + ": " + s + " = " + b + ". ");
+            i++;
+        }
+
+        switch(fac.getState()){
+            case RESTING:
+                String resting = "RESTING";
+                oStream.println(i + ": " + "State - " + resting + ".");
+            break;
+
+            case BROKEN:
+                String broken = "BROKEN";
+                oStream.println(i + ": " + "State - " + broken + ".");
+            break;
+
+            case WORKING:
+                String working = "WORKING";
+                oStream.println(i + ": " + "State - " + working + ".");
+            break;
+
+            case MAINTENANCE:
+                String maintenance = "MAINTENANCE";
+                oStream.println(i + ": " + "State - " + maintenance + ".");
+            break;
+        }
+
+        oStream.close();
+        System.out.println("File has been saved.");
+    }
+}
---
+++ marsmission/management/Finance.java
@@ -0,0 +1,31 @@
+/*
+    We didn't write this class as of yet, but we have plans to use it for the
+    upcoming projects
+*/
+package marsmission.management;
+
+public class Finance
+{
+    private static int interval, ratePerHour, usageRate, maintenanceCost, calcDowntimeFacility,
+    calcMaintCostFacility, calcUsageRate;
+
+    public int calcUsageRate()
+    {
+        return calcUsageRate;
+    }
+
+    public int maintenanceCost()
+    {
+        return maintenanceCost;
+    }
+
+    public int calcMaintCostFacility()
+    {
+        return calcMaintCostFacility;
+    }
+
+    public int calcDowntimeFacility()
+    {
+        return calcDowntimeFacility;
+    }
+}
