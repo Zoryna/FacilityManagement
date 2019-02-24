555b94d3799cf9c12cbe6696fbc444c024696630
Lucas Perez <knightfication@gmail.com>
2019-02-23 18:21:24 -0600

    Update on Driver

--- Driver.java
+++ Driver.java
@@ -2,16 +2,24 @@ import marsmission.habcontrol.*;
 import marsmission.dataaccess.*;
 import marsmission.simulation.*;
 import marsmission.management.*;
+import java.util.*;
 
 public class Driver {
     public static void main(String[] args){
         Facility apollo = new Facility("Apollo", 0);
         Facility nyx = new Facility("Nyx", 1);
-        Facility erebos = new Facility("Erebos", 2);
-        Facility athena = new Facility("Athena", 3);
-        Facility artemis = new Facility("Artemis", 4);
-        StateMachine sm = new StateMachine();
-        Control c = new Control();
-        TimeController tc = new TimeController();
+
+        if (apollo == nyx){
+            System.out.println("TRUE. The objects are the same.");
+        } else {
+            System.out.println("FALSE. The objects are different.");
+        }
+
+        if (apollo.getName() == nyx.getName()){
+            System.out.println("TRUE. The names are the same.");
+        } else {
+            System.out.println("FALSE. The names are different.");
+        }
+
     }
 }
--- marsmission/test/TestFacility.java
+++ marsmission/test/TestFacility.java
@@ -6,11 +6,22 @@ import java.util.*;
 public class TestFacility{
     public static void main (String[] args){
         Facility fac = new Facility("Airlocks #0", 0);
+        Facility fac0 = new Facility("Airlocks #2", 2);
+
+        // Stages the first object
         fac.setAddress("Acidalia Planitia");
         fac.setDescription("Controls the opening/closing of airlocks");
         fac.setCapacity(0);
         fac.setCost(100);
         fac.setProblemRate(5);
+
+        // Checks if objects are unique
+        if (fac == fac0){
+            System.out.println("TRUE");
+        } else {
+            System.out.println("FALSE");
+        }
+
 
         System.out.println("getName: " + fac.getName());
         System.out.println("getAddress: " + fac.getAddress());
