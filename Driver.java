ebb336c406027eac2088c24da691f6f33146384c
Lucas Perez <knightfication@gmail.com>
2019-02-23 20:13:56 -0600

    Driver updated

--- Driver.java
+++ Driver.java
@@ -6,20 +6,34 @@ import java.util.*;
 
 public class Driver {
     public static void main(String[] args){
+
+        // Staging Objects
         Facility apollo = new Facility("Apollo", 0);
         Facility nyx = new Facility("Nyx", 1);
+        Facility erebos = new Facility("Erebos", 2);
+        Facility athena = new Facility("Athena", 3);
+        Facility artemis = new Facility("Artemis", 4);
+        Facility[] ar = new Facility[5];
+        ar[0] = apollo;
+        ar[1] = nyx;
+        ar[2] = erebos;
+        ar[3] = athena;
+        ar[4] = artemis;
 
-        if (apollo == nyx){
-            System.out.println("TRUE. The objects are the same.");
-        } else {
-            System.out.println("FALSE. The objects are different.");
+        // Staging Time of the Day
+        TimeController tc = new TimeController();
+        tc.setHours(7);
+
+        // Checking what the Facilities are doing at 7 am
+        System.out.println("This is the state of affairs at 7 am:");
+        for (int i = 0; i < ar.length; i++){
+            System.out.println(ar[i].getName() + " is " + ar[i].getState());
         }
 
-        if (apollo.getName() == nyx.getName()){
-            System.out.println("TRUE. The names are the same.");
-        } else {
-            System.out.println("FALSE. The names are different.");
+        // Putting all the machines facility to work
+        FacilityMachines fm = FacilityMachines();
+        for (int i = 0; i < ar.length; i++){
+            // Show Machines for every facility
         }
-
     }
 }
