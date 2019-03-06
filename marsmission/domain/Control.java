2cce0b59752fb1bba9cae14123448e2b034f601d
Lucas Perez <knightfication@gmail.com>
2019-03-06 13:40:10 -0600

    Fetching everything from Master

---
+++ marsmission/domain/Control.java
@@ -0,0 +1,20 @@
+package marsmission.domain;
+
+public class Control extends StateMachine {
+
+    public void assignToUse(Facility fac){
+        setStateWorking(fac);
+    }
+
+    public void vacateFacility(Facility fac){
+        setStateResting(fac);
+    }
+
+    public void scheduleMaintenance(Facility fac){
+        setStateMaintenance(fac);
+    }
+
+    public void shitIsBroken(Facility fac){
+        setStateBroken(fac);
+    }
+}
