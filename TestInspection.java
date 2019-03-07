0a2dd040c62d735da7c9dbc4d819acf3069f91c4
Zoryna <jsoryna@gmail.com>
2019-03-07 14:44:02 -0600

    Switched index for 2 of the machines to test the array

--- TestInspection.java
+++ TestInspection.java
@@ -9,35 +9,32 @@ public class TestInspection
         Machines mach1 = new Machines(fac1);
 
         Inspection ins = new Inspection();
-        StateMachine sm = new StateMachine();
+        Control c = new Control();
 
         fac0.setName("Fac0");
         ins.setFacility(fac0);
-        System.out.println("This is facility:  " + fac0 + " and the name is " + fac0.getName() + " and the Inspection's facility is " + ins.getFacility()); //check if they have the same facilityy
-        System.out.println("The current state is: " + fac0.getState()); //at resting state
+        //System.out.println("This is facility:  " + fac0 + " and the name is " + fac0.getName() + " and the Inspection's facility is " + ins.getFacility()); //check if they have the same facilityy
+
 
         mach0.startMachines();
 
         System.out.println("Changing some machines to false");
-        mach0.oxygenator(true);
-        mach0.innerAirlocks(false); //fix
-        mach0.externalAirlocks(true); //fix
-        mach0.nuclearReactor(false); //fix
+        mach0.oxygenator(false);
+        mach0.innerAirlocks(true); //fix, index 1
+        mach0.externalAirlocks(true); //fix, index 2
+        mach0.nuclearReactor(true); //fix, index 3
         mach0.comms(false);
         mach0.waterMaking(false);
 
         mach0.initializeMap(mach0.getMap());
-        //mach0.readMachines();
 
         System.out.println("Show status again through getFacilityStatus");
         mach0.getFacilityStatus(mach0.getMap()); //testing
 
-
         System.out.println("Show which machines are not working");
         ins.returnBrokenMachines(mach0.getMap(), mach0.getMachines()); //fix
-        System.out.println("Testing when putting listFacilites after");
-
-        ins.listFacilityIssues(mach0.getMap(), mach0.getMachines());
+        //System.out.println("Testing when putting listFacilites after");
+        //ins.listFacilityIssues(mach0.getMap(), mach0.getMachines());
 
 
         /*if ((ins.checkMachines(mach0.getMap())) > 0) //if at least 1 is not working
@@ -49,7 +46,7 @@ public class TestInspection
             System.out.println("All the machines are working");
         }
 
-        sm.setStateBroken(fac0);
+        c.shitIsBroken(fac0);
         if (ins.isBroken() == true)
             System.out.println("This facility is broken");
         else
