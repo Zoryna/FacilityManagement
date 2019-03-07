37e858895e6f3de2d104a41433745fe032a650d8
Zoryna <jsoryna@gmail.com>
2019-03-07 12:29:25 -0600

    Testing setting some machine to false

--- TestInspection.java
+++ TestInspection.java
@@ -8,12 +8,14 @@ public class TestInspection
         Machines mach0 = new Machines(fac0);
         Machines mach1 = new Machines(fac1);
 
-        Inspection ins = new Inspection();
+        //Inspection ins = new Inspection();
 
-        ins.setFacility(fac0);
         fac0.setName("Fac0");
-        System.out.println("This is facility " + fac0.getName() + " and the Inspection's facility is " + ins.getFacility());
+        //ins.setFacility(fac0);
+        System.out.println("This is facility " + fac0.getName()); //+ " and the Inspection's facility is " + ins.getFacility());
         System.out.println("The current state is: " + fac0.getState()); //at resting state
+
+        mach0.startMachines();
 
         mach0.oxygenator(true);
         mach0.innerAirlocks(false);
@@ -22,15 +24,17 @@ public class TestInspection
         mach0.comms(true);
         mach0.waterMaking(false);
 
+        mach0.getFacilityStatus();
 
-        if ((ins.checkMachines(mach0.getMap())) > 0) //if at least 1 is not working
+
+        /*if ((ins.checkMachines(mach0.getMap())) > 0) //if at least 1 is not working
         {
             System.out.println("At least one of the machines is broken");
         }
         else
         {
             System.out.println("All the machines are working");
-        }
+        } */
 
 
     }
