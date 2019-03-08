492ef191fdb6380b10319ee661e4b65b5b738b6c
Zoryna <jsoryna@gmail.com>
2019-03-07 21:15:07 -0600

    Testing what happens when a facility makes 2 maintenance requests

--- Maintenance.java
+++ Maintenance.java
@@ -2,39 +2,45 @@ import java.util.*;
 
 public class Maintenance  {
 
+  private Facility fac;
   private Inspection ins;
+  private Machines mach;
 
-
-  public void listMaintenanceRequest(Inspection ins, boolean[] machines){ //shows the request, which says what needs to be fixed from that inspection
+  public void listMaintenanceRequest(int reqNum){ //shows the request, which says what needs to be fixed from that inspection
 
     System.out.println("This is facility " + fac.getName());
-    ins.makeMaintenanceRequest(fac);
+    ins.makeMaintenanceRequest(reqNum);
 
-    // Shows what is broken in this maintenance request
-    ins.listFacilityIssues(fac, machines);
+    //Shows what is broken in this maintenance request
+    ins.returnBrokenMachines(mach.getMap(), mach.getMachines());
   }
 
-  public void fixMachines(Map<String, Boolean> map){ // changes broken machines to working (true)
+  /*public void fixMachines(Map<String, Boolean> map){ //changes broken machines to true
 
     map.replaceAll((key, value) -> true);
     System.out.println("Machines are now fixed");
 
     /*for (Map.Entry<String, Boolean> i : map.entrySet()) {
-      System.out.println(i.getKey() + ": " + i.getValue()); */
+      System.out.println(i.getKey() + ": " + i.getValue());
 
 
-    }
-  }
+    } */
 
-  public Facility fixFacility(Facility fac, Control c){ // changes the faciity state to resting
+  public Facility fixFacility(Control c){ //changes the faciity state to resting
 
-    if ( (fac.state == State.MAINTENANCE) || (fac.state == State.BROKEN) ){
+    if (fac.getState().toString().equals("BROKEN"))
+    {
       System.out.println("This state is broken, it is going to change to RESTING");
       c.vacateFacility(fac);
       System.out.println("The state is now " + fac.getState());
       return fac;
-    } else {
+    }
+    else
+    {
       return fac;
     }
   }
+
+
 }
+
--- TestMaintenance.java
+++ TestMaintenance.java
@@ -2,7 +2,40 @@ public class TestMaintenance
 {
     public static void main (String args[])
     {
+        Facility apollo = new Facility();
+        Machines apolloMach = new Machines(apollo);
 
+        Inspection ins = new Inspection();
+        Control c = new Control();
+
+        apollo.setName("Apollo");
+        ins.setFacility(apollo);
+
+        apolloMach.startMachines(); //all machines start as true
+        apolloMach.getFacilityStatus(); //show status of all machines
+
+        apolloMach.oxygenator(false);
+        apolloMach.innerAirlocks(false);
+        apolloMach.externalAirlocks(false);
+        apolloMach.nuclearReactor(false);
+        apolloMach.comms(false);
+        apolloMach.waterMaking(false);
+
+        apolloMach.update();
+        apolloMach.getFacilityStatus();
+
+        //should only show broken machines
+        System.out.println("Here are the broken machines");
+        ins.returnBrokenMachines(apolloMach.getMap(), apolloMach.getMachines());
+
+        //testing the requests
+        c.shitIsBroken(apollo);
+        if(ins.makeMaintenanceRequest(1) > 0)
+        {
+            System.out.println("This facility will go through maintenance");
+        }
+        else
+            System.out.println("This facility will not go through maintenance");
 
 
 
