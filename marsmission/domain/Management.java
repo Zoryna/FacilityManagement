9ff14e797d641ef5952f63f388fb44116559946c
Zoryna <jsoryna@gmail.com>
2019-03-10 13:25:46 -0500

    Fixed some print statements in Management, and used Management in Driver

--- marsmission/domain/Management.java
+++ marsmission/domain/Management.java
@@ -8,7 +8,7 @@ public class Management
 
         double percentageWorking = (workingMachines * 100)/6; //calculates the percentage of facility working
         System.out.println("The facility is %" + percentageWorking + " working");
-        System.out.println(workingMachines + " machines are actually being used");
+        System.out.println(workingMachines + " machines are actually working");
     }
 
     public void problemRateFacility(double amountTests, double amountFail) //percentage of the tests that failed
@@ -22,7 +22,7 @@ public class Management
     public void requestAvailableCapacity(double workingMachines) //the remaining percentage of the facility
     {
         double percentageWorking = (workingMachines * 100)/6; //calculates the percentage of facility working
-        System.out.println(percentageWorking + "% of the Facility is working");
+        System.out.println(percentageWorking + "% of the Facility is in use");
 
         double availableCapacity = 100 - percentageWorking;
         System.out.println(availableCapacity + "% of the Facility is available to request use");
