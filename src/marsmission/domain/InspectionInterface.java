0d9ffb4f61599a8bd5325d4d8031edc3c3d98a65
Zoryna <jsoryna@gmail.com>
2019-03-24 14:41:17 -0500

    Added the setters and getters in the Inspection and Maintenance interfaces

--- src/marsmission/domain/InspectionInterface.java
+++ src/marsmission/domain/InspectionInterface.java
@@ -3,6 +3,20 @@ import java.util.*;
 
 public interface InspectionInterface {
 
+    //setters
+    public void setFacility(Facility fac);
+
+    public void setMachines(Machines mach);
+
+    public void setMaintenance(Maintenance maint);
+
+    //getters
+    public Facility getFacility();
+
+    public Machines getMachines();
+
+    public Maintenance getMaintenance();
+
     public boolean isBroken();
 
     public boolean makeMaintenanceRequest();
--- src/marsmission/domain/MaintenanceInterface.java
+++ src/marsmission/domain/MaintenanceInterface.java
@@ -4,6 +4,16 @@ import java.util.*;
 
 public interface MaintenanceInterface {
 
+    //setter
+    public void setInspection(Inspection ins);
+
+    public void setControl(Control c);
+
+    //getter
+    public Inspection getInspection();
+
+    public Control getControl();
+
     public void listMaintenanceRequest(Map<String, Boolean> map);
 
     public void fixMachines(Map<String, Boolean> map);
