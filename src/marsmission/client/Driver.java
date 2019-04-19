c7dfdc5c2d9189dc51f76fae4a2b69f887e53ef9
Zoryna <jsoryna@gmail.com>
2019-04-19 16:46:44 -0500

    Have VisitorImpl say the status of each machine

--- src/marsmission/client/Driver.java
+++ src/marsmission/client/Driver.java
@@ -22,6 +22,7 @@ public class Driver {
         Maintenance maint = (Maintenance) context.getBean("maintenance");
         Management man = (Management) context.getBean("management");
         StatesFaçade sf = (StatesFaçade) context.getBean("statesFaçade");
+        VisitorImpl v = (VisitorImpl) context.getBean("visitorImpl");
 
         System.out.println("\n");
         apollo.setStateWorking(apollo); // Looks weird, but it's not about looks, babe
@@ -38,72 +39,34 @@ public class Driver {
         apollo.getInnerAirlocks().setStatus(true);
         apollo.getExternalAirlocks().setStatus(true);
         apollo.getNuclearReactor().setStatus(true);
-        apollo.getComms().setStatus(true);
+        apollo.getComms().setStatus(false);
         apollo.getWaterExtraction().setStatus(true);
         apollo.getOxygenator().setFacility(apollo);
         apollo.startUpdateMap();
-        System.out.println("Apollo's Oxygenator is: " + apollo.getOxygenator().status());
+        /*System.out.println("Apollo's Oxygenator is: " + apollo.getOxygenator().status());
         System.out.println("Apollo's Inner Airlock is: " + apollo.getInnerAirlocks().status());
         System.out.println("Apollo's External Airlock is: " + apollo.getExternalAirlocks().status());
         System.out.println("Apollo's Nuclear Reactor is: " + apollo.getNuclearReactor().status());
         System.out.println("Apollo's Comms is: " + apollo.getComms().status());
-        System.out.println("Apollo's WaterExtraction is: " + apollo.getWaterExtraction().status());
-
-        System.out.println("\n");
-
-        Inspection i = maint.getInspection();
-        apollo.addObserver(i);
-        i.setFacility(apollo);
-        i.returnBorkenMachines(apollo.getMap());
-        i.update();
-        sf.getStateBroken().setStateBroken(apollo);
-
-        apollo.hasChanged();
-        apollo.setChanged();
-        apollo.notify(i);
-
-        System.out.println("Testing Maintenance");
-        maint.setInspection(i);
-        maint.setStatesFaçade(sf);
-        maint.listMaintenanceRequest(i.returnBorkenMachines(apollo.getMap()));
-        System.out.println("\n");
-
-        System.out.println("Fixing the machines:");
-        maint.fixMachines(apollo.getMap());
-        maint.fixFacility();
+        System.out.println("Apollo's WaterExtraction is: " + apollo.getWaterExtraction().status()); */
 
 
-        System.out.println("\n");
-        System.out.println("Testing Management on Apollo");
-        man.actualUsage(1);
-        man.problemRateFacility(6, 1);
-        man.requestAvailableCapacity(1);
 
-        System.out.println("\n");
 
-        System.out.println("Calculating Apollo's finances");
-        Finance f = man.getFinance();
-        apollo.addObserver(f);
-        f.setRatePerHour(3.05);
-        f.setMaintHourlyCost(50);
-        f.calcUsage(1, 8);
-        f.calcMaintCostFacility(1, 5);
-        f.calcDowntimeFacility(6);
-        f.update();
 
-        apollo.hasChanged();
-        apollo.setChanged();
-        apollo.notify(f);
 
-        System.out.println("\n");
-        System.out.println("Removing the Observers");
-        apollo.deleteObserver(i);
-        apollo.deleteObserver(f);
 
-        if (apollo.checkObserver() == false)
-            System.out.println("The Observers have been removed");
-        else
-            System.out.println("The Observers have not been removed");
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
--- src/marsmission/domain/VisitorImpl.java
+++ src/marsmission/domain/VisitorImpl.java
@@ -3,27 +3,45 @@ package marsmission.domain;
 public class VisitorImpl implements Visitor{
 
     public void visit(Comms c) {
-        System.out.println(c.getMachine() + " is 5 years old");
+        if (c.status() == true)
+            System.out.println(c.getMachine() + "is working. It's status is " + c.status());
+        else
+            System.out.println(c.getMachine() + " is NOT working. It's status is " + c.status());
     }
 
     public void visit(ExternalAirlocks e) {
-        System.out.println(e.getMachine() + " is 8 years old");
+        if (e.status() == true)
+            System.out.println(e.getMachine() + "is working. It's status is " + e.status());
+        else
+            System.out.println(e.getMachine() + " is NOT working. It's status is " + e.status());
     }
 
     public void visit(NuclearReactor n) {
-        System.out.println(n.getMachine() + " is 3 years old");
+        if (n.status() == true)
+            System.out.println(n.getMachine() + "is working. It's status is " + n.status());
+        else
+            System.out.println(n.getMachine() + " is NOT working. It's status is " + n.status());
     }
 
     public void visit(WaterExtraction w) {
-        System.out.println(w.getMachine() + " is 9 years old");
+        if (w.status() == true)
+            System.out.println(w.getMachine() + "is working. It's status is " + w.status());
+        else
+            System.out.println(w.getMachine() + " is NOT working. It's status is " + w.status());
     }
 
     public void visit(Oxygenator o) {
-        System.out.println(o.getMachine() + " is 10 years old");
+        if (o.status() == true)
+            System.out.println(o.getMachine() + "is working. It's status is " + o.status());
+        else
+            System.out.println(o.getMachine() + " is NOT working. It's status is " + o.status());
     }
 
     public void visit(InnerAirlocks i) {
-        System.out.println(i.getMachine() + " is 4 years old");
+        if (i.status() == true)
+            System.out.println(i.getMachine() + "is working. It's status is " + i.status());
+        else
+            System.out.println(i.getMachine() + " is NOT working. It's status is " + i.status());
     }
 
 }
