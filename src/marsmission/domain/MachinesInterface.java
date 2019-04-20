744dcf83ac6ae9c5dd71911e780da69462b7acdd
Zoryna <jsoryna@gmail.com>
2019-04-20 11:22:12 -0500

    Revised the UML and added setStatus() method in MachinesInterface because it was missing

--- src/marsmission/domain/MachinesInterface.java
+++ src/marsmission/domain/MachinesInterface.java
@@ -7,6 +7,7 @@ public interface MachinesInterface {
     public Facility getFacility(); // Returns the Facility to which this machine was assigned to
     public void setFacility(Facility fac); // Sets a Facility to a Machine
     public void machine(); // This is what the specific implementation of a machine should do (aka oxygenator, etc)
+    public void setStatus(boolean s);
     public boolean status(); // Is the machine working?
     public String getMachine();
 }
