f89d40294212d570bacdafac5765b9922540afe6
Lucas Perez <knightfication@gmail.com>
2019-04-20 10:14:56 -0500

    Just changing the string in Visitor a bit

--- src/marsmission/domain/VisitorImpl.java
+++ src/marsmission/domain/VisitorImpl.java
@@ -3,7 +3,6 @@ package marsmission.domain;
 public class VisitorImpl implements Visitor{
 
     public void visit(MachinesInterface i) {
-        System.out.println(i.getMachine() + " is 5 years old");
+        System.out.println(i.getMachine() + " is 32 hours old");
     }
-
 }
