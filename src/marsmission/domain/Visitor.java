605bc839c53e322a97ef785da57430a700f4c7c2
Lucas Perez <knightfication@gmail.com>
2019-04-20 10:13:33 -0500

    Adding Liskov Substitution Principle to Visitor

--- src/marsmission/domain/Visitor.java
+++ src/marsmission/domain/Visitor.java
@@ -1,17 +1,5 @@
 package marsmission.domain;
 
 public interface Visitor {
-
-    public void visit(Comms c);
-
-    public void visit(ExternalAirlocks e);
-
-    public void visit(NuclearReactor n);
-
-    public void visit(WaterExtraction w);
-
-    public void visit(Oxygenator o);
-
-    public void visit(InnerAirlocks i);
-
+    public void visit(MachinesInterface i);
 }
